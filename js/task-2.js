13c69ec08e78f1acccf00a9a10868568a8823e9b
TarasNovik <148384148+TarasNovik@users.noreply.github.com>
2024-08-21 22:18:09 +0200

    js

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,22 @@
+class Storage {
+    #items = [];
+    constructor(parametr) {
+        this.#items = parametr;
+    }
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        return this.#items.push(newItem);
+
+    }
+    removeItem(itemToRemove) {
+        const newMassiv = this.#items.filter((item) => item !== itemToRemove);
+        this.#items = newMassiv;
+    }
 
 
-
-
-
-
-
+}
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
@@ -17,3 +29,4 @@ console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 storage.removeItem("Scaner");
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,4 @@
-
-
+class StringBuilder { initialValue }
 
 
 
